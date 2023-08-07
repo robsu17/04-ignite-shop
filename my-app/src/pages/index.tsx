0f2a5de35ac9ac3b7bdc554ab3c3d606528d51d8
Robson Wendel <robson.costa134@gmail.com>
2023-08-07 09:45:28 -0300

    update card img

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,3 +1,28 @@
+import Image from 'next/image'
+import { HomeContainer, Product } from '@/styles/pages/home'
+
+import camiseta1 from '../assets/1.png'
+import camiseta2 from '../assets/2.png'
+import camiseta3 from '../assets/3.png'
+
 export default function Home() {
-  return <h1></h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
