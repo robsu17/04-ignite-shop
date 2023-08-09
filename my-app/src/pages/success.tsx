f6a0eedbfaae7a9fc3e8a9f8534089caec38a03b
Robson Wendel <robson.costa134@gmail.com>
2023-08-09 00:04:02 -0300

    update page success

--- my-app/src/pages/success.tsx
+++ my-app/src/pages/success.tsx
@@ -1,3 +1,18 @@
+import { SuccessContainer, ImageContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul Diego Fernandes, sua compra de 3 camisetas já está a caminho da
+        sua casa.{' '}
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
